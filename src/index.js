4a9af1cfc3e0d3067205ce4e69621649e85e5f0f
Alla Borisova <alla.borisova27@gmail.com>
2022-01-05 12:22:18 +0500

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function toReadable (number) {
-  
-}
+
+	let readableString = '';
+	let arrayNames = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+	let arrayNamesTen = ['','', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+	if (number == 0) {return 'zero';}
+
+	let firstNumber = parseInt(number / 100);
+	if (firstNumber > 0){
+		readableString = arrayNames[firstNumber] + ' hundred';
+		
+	}
+
+	let secondNumber = number % 100;
+	if (secondNumber > 19){
+		let tensTen = parseInt(secondNumber / 10);
+		readableString = readableString + ' ' + arrayNamesTen[tensTen];
+
+	} else if (secondNumber >= 10){
+		readableString = readableString + ' ' + arrayNames[secondNumber];
+    	return readableString.trim();
+	}
+
+	let thirdNumber = number % 10;
+	readableString = readableString + ' ' + arrayNames[thirdNumber];
+	
+	return readableString.trim();
+  }
